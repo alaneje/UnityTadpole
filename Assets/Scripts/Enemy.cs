51f80cc2ab04dbb5dede05de743e062885431015
Alan E.J Easton <alaneje@users.noreply.github.com>
2023-10-09 11:04:59 +0100

    Update Enemy.cs
    fixed issue where rigidbody would not pass because it wasn't set intime.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,7 +7,7 @@ public class Enemy : MonoBehaviour
     public bool OnCollisionDamage;
     Rigidbody2D MyRigid;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         MyRigid = this.gameObject.GetComponent<Rigidbody2D>();
         
