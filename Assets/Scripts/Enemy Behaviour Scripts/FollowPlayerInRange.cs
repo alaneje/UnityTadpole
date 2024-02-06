e83ae0d08d98255d7bf17320e7989e1bc3312a78
Alan E.J Easton <alaneje@users.noreply.github.com>
2024-02-06 22:49:21 +0000

    missile file
    - Missile Fly updated to move towards player
    -VS Code packages updated

--- Assets/Scripts/Enemy Behaviour Scripts/FollowPlayerInRange.cs
+++ Assets/Scripts/Enemy Behaviour Scripts/FollowPlayerInRange.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class FollowPlayerInRange : MonoBehaviour
 {
-    
+    public PlayerManager player;
+    public float AcceptedRange;
+    public float Speed;
     
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerManager>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(PlayerInRange()){
+            Debug.Log("PLAYER IN RANGE.");
+            this.gameObject.transform.position =  Vector3.MoveTowards(this.gameObject.transform.position , player.transform.position, Time.deltaTime * Speed);
+        }
     }
+
+    bool PlayerInRange(){
+        if(Vector3.Distance(this.gameObject.transform.position,player.gameObject.transform.position) < AcceptedRange){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+        
+    }//Looks at if a player is in range to the object
 }
